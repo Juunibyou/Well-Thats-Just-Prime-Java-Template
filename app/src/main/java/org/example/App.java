e80960030baff583640049b397df0420f2f52f42
Juunibyou <danijunn@gmail.com>
2025-02-02 17:06:57 -0600

    Final

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,22 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    // Test your Factorizer class using a driver program here!
+
+    //prompt and user input
+    System.out.println("|_____Hello! Welcome to the factoring program!_____|\n");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What would you like to factor?");
+    int input = scanner.nextInt();
+    scanner.close();
+
+    //array list of prime factors
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> factors = factorizer.primeFactors(input);
+
+    //output array list of final result
+    System.out.println("The factor of " + input + " would be: " + factors);
   }
 }
--- app/src/main/java/org/example/Factorizer.java
+++ app/src/main/java/org/example/Factorizer.java
@@ -3,8 +3,28 @@ package org.example;
 import java.util.ArrayList;
 
 public class Factorizer {
-  public ArrayList<Integer> primeFactors(Integer n) {
-    // Implement your code here!
-    return new ArrayList<>();
+  public ArrayList<Integer> primeFactors(int n) {
+    ArrayList<Integer> factors = new ArrayList<Integer>();
+
+    //adding factors into array as long as they a divisable by 2
+    while(n%2 == 0){
+      factors.add(2);
+      n = n/2;
+    }
+    
+    //checking if it's factorable with other numbers
+    for(int i = 3; i < n; i++){
+      while(n%i == 0){
+        factors.add(i);
+        n = n/i;
+      }
+    }
+
+    //adding any remaining numbers not divisable
+    if(n > 2){
+      factors.add(n);
+    }
+
+    return factors;
   }
 }
