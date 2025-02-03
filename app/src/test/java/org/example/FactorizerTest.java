aa8268872508fec988e2ffb11191f502a05278e3
Juunibyou <danijunn@gmail.com>
2025-02-02 18:32:28 -0600

    Updated Final

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -8,6 +8,30 @@ class FactorizerTest {
   @Test
   void itReturnsAnEmptyListForZero() {
     Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+    assertEquals(factorizer.primeFactors(0), new ArrayList<>());
+  }
+
+  @Test
+  void itReturnsAnEmptyListForOne(){
+    Factorizer factorizer = new Factorizer();
+    assertEquals(factorizer.primeFactors(1), new ArrayList<>());
+  }
+
+  @Test
+  void itReturnsAListFor10(){
+    Factorizer factorizer = new Factorizer();
+    ArrayList<Integer> factors = new ArrayList<>();
+    factors.add(2);
+    factors.add(5);
+    assertEquals(factorizer.primeFactors(10), factors);
+  }
+
+  @Test
+  void itReturnsAListFor25(){
+    ArrayList<Integer> factors = new ArrayList<>();
+    Factorizer factorizer = new Factorizer();
+    factors.add(5);
+    factors.add(5);
+    assertEquals(factorizer.primeFactors(25), factors);
   }
 }
